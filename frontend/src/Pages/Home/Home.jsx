4567382cc312f5fc32ed622a9e31f845ff224680
Aditya Sikarwar <asikarwar792@gmail.com>
2025-08-15 22:31:35 +0530

    import changes

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import "./Home.css";
 import Header from '../../Components/Header/Header';
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
-import FoodDisplay from '../../Components/foodDisplay/foodDisplay';
+// import FoodDisplay from '../../Components/foodDisplay/foodDisplay';
 import AppDownload from '../../Components/AppDownload/AppDownload';
-
+import FoodDisplay from "../../Components/foodDisplay/foodDisplay.jsx"
 const Home = () => {
   const [category,setCategory]=useState("All");
   return (
